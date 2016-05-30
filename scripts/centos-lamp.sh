07c5945defe1319a0415fff9897a00e6ec53a35d
Simon Owusu <zx8086@mac.com>
2016-05-30 02:25:05 +0200

    adding files for LAMP Stock

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Updates CentOS with any patches
+yum update -y --exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /Vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+#MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysql start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+service httpd restart
